01e75ed44033e13343d5f2f014db59e8b12ea338
Alexmwale <alexmwalec03@gmail.com>
2026-04-02 01:01:42 +0200

    Application initialization

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AttendanceApp());
+}
+
+class AttendanceApp extends StatelessWidget {
+  const AttendanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Attendance System',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Automated Attendance System'),
+      ),
+      body: const Center(
+        child: Text(
+          "Project Setup Complete Team,Let's build it",
+          style: TextStyle(fontSize: 26),
+        ),
+      ),
+    );
+  }
+}
