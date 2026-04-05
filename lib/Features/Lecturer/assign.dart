4e0b3857552cfec5797237080929a8af7e5a3b4a
Alexmwale <alexmwalec03@gmail.com>
2026-04-06 00:36:10 +0200

    Assign Invigilator initial UI

---
+++ lib/Features/Lecturer/assign.dart
@@ -0,0 +1,7 @@
+import 'package:automated_attendance_system/Features/Lecturer/invigilator_dashboard.dart';
+import 'package:flutter/material.dart';
+
+class assign extends StatelessWidget{
+    const assign({super.key});
+    
+}
