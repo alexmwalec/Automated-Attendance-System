ba05426a31a236315aac7f92e78ee10a191190da
Alexmwale <alexmwalec03@gmail.com>
2026-04-06 00:42:21 +0200

    Fixing SizeBox

--- lib/Features/Lecturer/assign.dart
+++ lib/Features/Lecturer/assign.dart
@@ -1,8 +1,8 @@
 import 'package:automated_attendance_system/Features/Lecturer/invigilator_dashboard.dart';
 import 'package:flutter/material.dart';
 
-class assign extends StatelessWidget{
-    const assign({super.key});
+class Assign extends StatelessWidget{
+    const Assign({super.key});
     @override
     Widget build(BuildContext context) {
       return Scaffold(
